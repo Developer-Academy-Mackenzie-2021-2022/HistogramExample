d1ed531c5c9523118ffd1190edcd7efa8e06ddaf
Carolina Ortega <cahortega@outlook.com>
2021-11-05 09:30:49 -0300

    Aplicativo modelo

--- HistogramExample/HistogramExample/ContentView.swift
+++ HistogramExample/HistogramExample/ContentView.swift
@@ -6,23 +6,44 @@
 //
 
 import SwiftUI
+import FrameworkDeGraficos
 
 struct ContentView: View {
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading) {
-                    Text("Histograma tipo 1")
-                        .font(.title2)
+                    Text("Histograma Simples")
+                        .font(.title) //bold
                         .padding()
-                    Rectangle()
-                        .frame(height: 300)
-                    Text("DescriçãoDescriçãoDescriçãoDescriçãoDescriçãoDescriçãoDescriçãoDescriçãoDescriçãoDescriçãoDescriçãoDescriçãoDescriçãoDescrição")
+                    Spacer()
+                    Histograma(data: [50, 30, 40, 70, 10, 80, 60],
+                               colors: [Color(red: 245/255, green: 69/255, blue: 79/255), //vermelho
+                                        Color(red: 239/255, green: 114/255, blue: 65/255), //laranja escuro
+                                        Color(red: 244/255, green: 150/255, blue: 63/255), //laranja claro
+                                        Color(red: 247/255, green: 197/255, blue: 99/255), //amarelo
+                                        Color(red: 146/255, green: 188/255, blue: 117/255), //verde
+                                        Color(red: 77/255, green: 168/255, blue: 141/255), //verde água
+                                        Color(red: 89/255, green: 119/255, blue: 142/255) //azul
+                                       ]
+                    ).frame(width: 400, height: 450)
+                    Text("Histograma com uma cor")
+                        .font(.title) //bold
                         .padding()
-                        .font(.headline)
-
+                    Histograma(data: [50, 30, 40, 70, 10, 80, 60],
+                               colors: [Color(red: 245/255, green: 69/255, blue: 79/255),
+                                        Color(red: 245/255, green: 69/255, blue: 79/255),
+                                        Color(red: 245/255, green: 69/255, blue: 79/255),
+                                        Color(red: 245/255, green: 69/255, blue: 79/255),
+                                        Color(red: 245/255, green: 69/255, blue: 79/255),
+                                        Color(red: 245/255, green: 69/255, blue: 79/255),
+                                        Color(red: 245/255, green: 69/255, blue: 79/255)
+                                       ]
+                    ).frame(width: 400, height: 450)
+                    
+                    
                 }
-            } .navigationTitle("Histograma")
+            }.navigationTitle("Histograma")
         }
     }
 }
