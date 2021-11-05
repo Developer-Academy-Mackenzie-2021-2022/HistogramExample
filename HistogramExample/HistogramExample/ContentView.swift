eb10de57a97425ef51bb881fe46b2ff60d5a89ea
Gui Reis <gui.sreis25@gmail.com>
2021-11-05 14:26:34 -0300

    Atualizando importação framework
    O framework mudou de nome e a função foi atualizada.

--- HistogramExample/HistogramExample/ContentView.swift
+++ HistogramExample/HistogramExample/ContentView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import FrameworkDeGraficos
+import Everest
 
 struct ContentView: View {
     var body: some View {
@@ -18,30 +18,30 @@ struct ContentView: View {
                         .padding()
                     Spacer()
                     Histograma(data: [50, 30, 40, 70, 10, 80, 60],
-                               colors: [Color(red: 245/255, green: 69/255, blue: 79/255), //vermelho
-                                        Color(red: 239/255, green: 114/255, blue: 65/255), //laranja escuro
-                                        Color(red: 244/255, green: 150/255, blue: 63/255), //laranja claro
-                                        Color(red: 247/255, green: 197/255, blue: 99/255), //amarelo
-                                        Color(red: 146/255, green: 188/255, blue: 117/255), //verde
-                                        Color(red: 77/255, green: 168/255, blue: 141/255), //verde água
-                                        Color(red: 89/255, green: 119/255, blue: 142/255) //azul
-                                       ]
+                               colors: [Color(red: 245/255, green: 69/255,  blue: 79/255),   // Vermelho
+                                        Color(red: 239/255, green: 114/255, blue: 65/255),   // Laranja escuro
+                                        Color(red: 244/255, green: 150/255, blue: 63/255),   // Laranja claro
+                                        Color(red: 247/255, green: 197/255, blue: 99/255),   // Amarelo
+                                        Color(red: 146/255, green: 188/255, blue: 117/255),  // Verde
+                                        Color(red: 77/255,  green: 168/255, blue: 141/255),  // Verde água
+                                        Color(red: 89/255,  green: 119/255, blue: 142/255)   // Azul
+                                       ],
+                               labelX: "Massa solar", labelY: "Número de estrelas"
                     ).frame(width: 400, height: 450)
                     Text("Histograma com uma cor")
                         .font(.title) //bold
                         .padding()
                     Histograma(data: [50, 30, 40, 70, 10, 80, 60],
-                               colors: [Color(red: 245/255, green: 69/255, blue: 79/255),
-                                        Color(red: 245/255, green: 69/255, blue: 79/255),
-                                        Color(red: 245/255, green: 69/255, blue: 79/255),
-                                        Color(red: 245/255, green: 69/255, blue: 79/255),
-                                        Color(red: 245/255, green: 69/255, blue: 79/255),
-                                        Color(red: 245/255, green: 69/255, blue: 79/255),
-                                        Color(red: 245/255, green: 69/255, blue: 79/255)
-                                       ]
+                               colors: [Color(red: 245/255, green: 69/255,  blue: 79/255),   // Vermelho
+                                        Color(red: 245/255, green: 69/255,  blue: 79/255),   // Vermelho
+                                        Color(red: 245/255, green: 69/255,  blue: 79/255),   // Vermelho
+                                        Color(red: 245/255, green: 69/255,  blue: 79/255),   // Vermelho
+                                        Color(red: 245/255, green: 69/255,  blue: 79/255),   // Vermelho
+                                        Color(red: 245/255, green: 69/255,  blue: 79/255),   // Vermelho
+                                        Color(red: 245/255, green: 69/255,  blue: 79/255),   // Vermelho
+                                       ],
+                               labelX: "Massa solar", labelY: "Número de estrelas"
                     ).frame(width: 400, height: 450)
-                    
-                    
                 }
             }.navigationTitle("Histograma")
         }
